d00ac3f58ea810f3d1210479defe0b6a25620829
realpolya <ps2780@columbia.edu>
2025-01-08 17:59:19 -0500

    create 3 tiles

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 /*-------------------------------- Constants --------------------------------*/
 
 const lineColor = "grey"
+const topColor = "hotpink"
+const leftColor = "purple"
+const rightColor = "aqua"
 
 /*-------------------------------- Cached Elements --------------------------------*/
 
@@ -20,18 +23,69 @@ const drawLine = (ctx, x1, y1, x2, y2, color="black", lineWidth=1) => {
 
 }
 
-const fillTop = (ctx, size, mid, start=0) => {
+const drawFillTop = (ctx, size, mid, start=0, strColor=lineColor, 
+    lineWidth=1, fillColor=topColor) => {
 
-    const end = start + size
+    const end = start + size;
 
     ctx.beginPath();
     ctx.moveTo(start, mid);
     ctx.lineTo(mid, end);
     ctx.lineTo(end, mid);
     ctx.lineTo(mid, start);
+    ctx.lineTo(start, mid);
+    ctx.strokeStyle = strColor;
+    ctx.lineWidth = lineWidth;
+    ctx.stroke();
     ctx.closePath();
 
-    ctx.fillStyle = "hotpink";
+    ctx.fillStyle = fillColor;
+    ctx.fill();
+
+}
+
+const fillLeft = (ctx, size, mid, start=0, fillColor=leftColor) => {
+
+    const end = start + size;
+    ctx.fillStyle = fillColor;
+
+    ctx.beginPath();
+    ctx.moveTo(start, mid);
+    ctx.lineTo(mid, end);
+    ctx.lineTo(start, end);
+    ctx.closePath();
+
+    ctx.fill();
+
+    ctx.beginPath();
+    ctx.moveTo(end, start);
+    ctx.lineTo(mid, start);
+    ctx.lineTo(end, mid);
+    ctx.closePath();
+
+    ctx.fill();
+
+}
+
+const fillRight = (ctx, size, mid, start=0, fillColor=rightColor) => {
+
+    const end = start + size;
+    ctx.fillStyle = fillColor;
+
+    ctx.beginPath();
+    ctx.moveTo(start, start);
+    ctx.lineTo(mid, start);
+    ctx.lineTo(start, mid);
+    ctx.closePath();
+
+    ctx.fill();
+
+    ctx.beginPath();
+    ctx.moveTo(end, end);
+    ctx.lineTo(end, mid);
+    ctx.lineTo(mid, end);
+    ctx.closePath();
+
     ctx.fill();
 
 }
@@ -41,31 +95,36 @@ const singleTile = (ctx, size, start=0) => {
     const mid = start + Math.floor(size / 2)
     const end = start + size
 
-    drawLine(ctx, start, mid, mid, end)
-    drawLine(ctx, start, mid, mid, start)
-    drawLine(ctx, mid, start, end, mid)
-    drawLine(ctx, mid, end, end, mid)
+    drawFillTop(ctx, size, mid)
 
     drawLine(ctx, start, start, start, end)
     drawLine(ctx, end, start, end, end)
 
-    fillTop(ctx, size, mid)
+    fillLeft(ctx, size, mid)
+    fillRight(ctx, size, mid)
 
 }
 
+const createCanvas = (parentEl) => {
+
+    const canvas = document.createElement('canvas');
+
+    canvas.style.backgroundColor = 'thistle';
+    canvas.width = 100;
+    canvas.height = 100;
+    canvas.style.height = '100px';
+    canvas.style.width = '100px';
+
+    parentEl.appendChild(canvas);
+
+    const context = canvas.getContext('2d')
+
+    singleTile(context, 100)
+
+}
 
 /*-------------------------------- Function calls --------------------------------*/
 
-const canvas = document.createElement('canvas');
-
-canvas.style.backgroundColor = 'thistle';
-canvas.width = 100;
-canvas.height = 100;
-canvas.style.height = '100px';
-canvas.style.width = '100px';
-
-canvasDiv.appendChild(canvas);
-
-const context = canvas.getContext('2d')
-
-singleTile(context, 100)
+createCanvas(canvasDiv)
+createCanvas(canvasDiv)
+createCanvas(canvasDiv)
