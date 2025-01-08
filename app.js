343c875d8e7a22c8a6af17487f8491e1d6fd91fe
realpolya <ps2780@columbia.edu>
2025-01-08 16:55:35 -0500

    create a drawing tool

--- app.js
+++ app.js
@@ -1,5 +1,24 @@
 const cubesEl = document.querySelectorAll('.cube')
 
 cubesEl.forEach(cube => {
-    cube.style.backgroundColor = "red"
+    cube.style.backgroundColor = "thistle"
 })
+
+// try canvas
+
+const canvasEl = document.getElementById('canvas')
+const context = canvasEl.getContext('2d')
+
+console.log("context ", context)
+
+const drawLine = (x1, y1, x2, y2, color="black", lineWidth=1) => {
+    context.beginPath();
+    context.moveTo(x1, y1);
+    context.lineTo(x2, y2);
+    context.strokeStyle = color;
+    context.lineWidth = lineWidth;
+    context.stroke();
+    context.closePath();
+}
+
+drawLine(5, 5, 300, 300)
