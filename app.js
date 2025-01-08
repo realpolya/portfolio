98d3bd22659301b45556a80173b3170b6c810536
realpolya <ps2780@columbia.edu>
2025-01-08 17:22:22 -0500

    remove table

--- app.js
+++ app.js
@@ -4,17 +4,10 @@ const lineColor = "grey"
 
 /*-------------------------------- Cached Elements --------------------------------*/
 
-const cubesEl = document.querySelectorAll('.cube')
 const canvasEl = document.getElementById('canvas')
 const canvasDiv = document.getElementById('canvases')
 
 /*-------------------------------- Functions --------------------------------*/
-
-cubesEl.forEach(cube => {
-    cube.style.backgroundColor = "thistle"
-})
-
-// try canvas
 
 const context = canvasEl.getContext('2d')
 
@@ -28,13 +21,19 @@ const drawLine = (ctx, x1, y1, x2, y2, color="black", lineWidth=1) => {
     ctx.closePath();
 }
 
+const singleTile = (ctx) => {
+    drawLine(ctx, 5, 5, 500, 300)
+}
+
 /*-------------------------------- Function calls --------------------------------*/
 
 drawLine(context, 5, 5, 500, 300)
 
 const canvas2 = document.createElement('canvas');
 canvas2.style.backgroundColor = lineColor;
+canvas2.style.height = '100px';
+canvas2.style.width = '100px';
 canvasDiv.appendChild(canvas2);
 const context2 = canvas2.getContext('2d')
 
-drawLine(context2, 5, 5, 500, 300)
+singleTile(context2)
