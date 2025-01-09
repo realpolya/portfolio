8b6514d71d25a217aa098c9ccbdc1c35b99ddbcf
realpolya <ps2780@columbia.edu>
2025-01-09 11:48:04 -0500

    start cube.js file

--- app.js
+++ app.js
@@ -1,3 +1,10 @@
+/*-------------------------------- Imports --------------------------------*/
+
+import { drawLine, drawFillTop, fillBottomLeft,
+    fillTopRight, fillLeft, fillBottomRight, 
+    fillTopLeft, fillRight, singleTile, leftHalfTile
+} from './js-files/cube.js'
+
 /*-------------------------------- Constants --------------------------------*/
 
 const lineColor = "grey"
@@ -5,146 +12,19 @@ const topColor = "#E0E1DD"
 const leftColor = "#778DA9"
 const rightColor = "#415A77"
 
+const colors = {
+    line: "grey",
+    top: "#E0E1DD",
+    left: "#778DA9",
+    right: "#415A77",
+}
+
 /*-------------------------------- Cached Elements --------------------------------*/
 
 const canvasDiv = document.getElementById('canvases')
 const secondCanvasDiv = document.getElementById('secondRow')
 
 /*-------------------------------- Functions --------------------------------*/
-
-const drawLine = (ctx, x1, y1, x2, y2, color="black", lineWidth=1) => {
-
-    ctx.beginPath();
-    ctx.moveTo(x1, y1);
-    ctx.lineTo(x2, y2);
-    ctx.strokeStyle = color;
-    ctx.lineWidth = lineWidth;
-    ctx.stroke();
-    ctx.closePath();
-
-}
-
-const drawFillTop = (ctx, size, mid, start=0, strColor=lineColor, 
-    lineWidth=1, fillColor=topColor) => {
-
-    const end = start + size;
-
-    ctx.beginPath();
-    ctx.moveTo(start, mid);
-    ctx.lineTo(mid, end);
-    ctx.lineTo(end, mid);
-    ctx.lineTo(mid, start);
-    ctx.lineTo(start, mid);
-    ctx.strokeStyle = strColor;
-    ctx.lineWidth = lineWidth;
-    ctx.stroke();
-    ctx.closePath();
-
-    ctx.fillStyle = fillColor;
-    ctx.fill();
-
-}
-
-const fillBottomLeft = (ctx, size, mid, end, start=0, fillColor=leftColor) => {
-    ctx.fillStyle = fillColor;
-
-    ctx.beginPath();
-    ctx.moveTo(start, mid);
-    ctx.lineTo(mid, end);
-    ctx.lineTo(start, end);
-    ctx.closePath();
-
-    ctx.fill();
-}
-
-const fillTopRight = (ctx, size, mid, end, start=0, fillColor=leftColor) => {
-    ctx.fillStyle = fillColor;
-
-    ctx.beginPath();
-    ctx.moveTo(end, start);
-    ctx.lineTo(mid, start);
-    ctx.lineTo(end, mid);
-    ctx.closePath();
-
-    ctx.fill();
-}
-
-const fillLeft = (ctx, size, mid, start=0, fillColor=leftColor) => {
-
-    const end = start + size;
-    fillBottomLeft(ctx, size, mid, end);
-    fillTopRight(ctx, size, mid, end);
-
-}
-
-const fillBottomRight = (ctx, size, mid, end, start=0, fillColor=rightColor) => {
-    ctx.fillStyle = fillColor;
-
-    ctx.beginPath();
-    ctx.moveTo(end, end);
-    ctx.lineTo(end, mid);
-    ctx.lineTo(mid, end);
-    ctx.closePath();
-
-    ctx.fill();
-}
-
-
-const fillTopLeft = (ctx, size, mid, end, start=0, fillColor=rightColor) => {
-    ctx.fillStyle = fillColor;
-
-    ctx.beginPath();
-    ctx.moveTo(start, start);
-    ctx.lineTo(mid, start);
-    ctx.lineTo(start, mid);
-    ctx.closePath();
-
-    ctx.fill();
-}
-
-
-const fillRight = (ctx, size, mid, start=0, fillColor=rightColor) => {
-
-    const end = start + size;
-    fillTopLeft(ctx, size, mid, end)
-    fillBottomRight(ctx, size, mid, end)
-
-}
-
-const singleTile = (ctx, size, start=0) => {
-
-    const mid = start + Math.floor(size / 2)
-    const end = start + size
-
-    drawFillTop(ctx, size, mid)
-
-    drawLine(ctx, start, start, start, end)
-    drawLine(ctx, end, start, end, end)
-
-    fillLeft(ctx, size, mid)
-    fillRight(ctx, size, mid)
-
-}
-
-// TODO:
-const leftHalfTile = (ctx, size, alternate=false, start=0) => {
-    const xSize = Math.floor(size / 2)
-    const xEnd = start + xSize
-    const xMid = start + Math.floor(size / 2)
-    const yMid = Math.floor(size / 2)
-
-    // top
-    ctx.fillStyle = leftColor;
-
-    ctx.beginPath();
-    ctx.moveTo(start, start);
-    ctx.lineTo(xEnd, yMid);
-    ctx.lineTo(xEnd, start);
-    ctx.closePath();
-
-    ctx.fill();
-
-}
 
 const createHalfCanvas = (parentEl) => {
 
@@ -160,7 +40,7 @@ const createHalfCanvas = (parentEl) => {
 
     const context = canvas.getContext('2d')
 
-    leftHalfTile(context, 100)
+    leftHalfTile(context, 100, colors)
 
 }
 
@@ -178,7 +58,7 @@ const createCanvas = (parentEl) => {
 
     const context = canvas.getContext('2d')
 
-    singleTile(context, 100)
+    singleTile(context, 100, colors)
 
 }
 
---
+++ js-files/cube.js
@@ -0,0 +1,164 @@
+const drawLine = (ctx, x1, y1, x2, y2, colors, lineWidth=1) => {
+
+    ctx.beginPath();
+    ctx.moveTo(x1, y1);
+    ctx.lineTo(x2, y2);
+    ctx.strokeStyle = colors.line;
+    ctx.lineWidth = lineWidth;
+    ctx.stroke();
+    ctx.closePath();
+
+}
+
+const drawFillTop = (ctx, size, mid, start=0, colors, 
+    lineWidth=1) => {
+
+    const end = start + size;
+
+    ctx.beginPath();
+    ctx.moveTo(start, mid);
+    ctx.lineTo(mid, end);
+    ctx.lineTo(end, mid);
+    ctx.lineTo(mid, start);
+    ctx.lineTo(start, mid);
+    ctx.strokeStyle = colors.line;
+    ctx.lineWidth = lineWidth;
+    ctx.stroke();
+    ctx.closePath();
+
+    ctx.fillStyle = colors.top;
+    ctx.fill();
+
+}
+
+const fillBottomLeft = (ctx, size, mid, end, start=0, colors) => {
+
+    ctx.fillStyle = colors.right;
+
+    ctx.beginPath();
+    ctx.moveTo(start, mid);
+    ctx.lineTo(mid, end);
+    ctx.lineTo(start, end);
+    ctx.closePath();
+
+    ctx.fill();
+
+}
+
+const fillTopRight = (ctx, size, mid, end, start=0, colors) => {
+    ctx.fillStyle = colors.right;
+
+    ctx.beginPath();
+    ctx.moveTo(end, start);
+    ctx.lineTo(mid, start);
+    ctx.lineTo(end, mid);
+    ctx.closePath();
+
+    ctx.fill();
+}
+
+
+const fillBottomRight = (ctx, size, mid, end, start=0, colors) => {
+    ctx.fillStyle = colors.left;
+    
+    ctx.beginPath();
+    ctx.moveTo(end, end);
+    ctx.lineTo(end, mid);
+    ctx.lineTo(mid, end);
+    ctx.closePath();
+    
+    ctx.fill();
+}
+
+
+const fillTopLeft = (ctx, size, mid, end, start=0, colors) => {
+
+    ctx.fillStyle = colors.left;
+    
+    ctx.beginPath();
+    ctx.moveTo(start, start);
+    ctx.lineTo(mid, start);
+    ctx.lineTo(start, mid);
+    ctx.closePath();
+    
+    ctx.fill();
+
+}
+
+const fillLeft = (ctx, size, mid, start=0, colors) => {
+
+    const end = start + size;
+    fillBottomLeft(ctx, size, mid, end, 0, colors);
+    fillTopRight(ctx, size, mid, end, 0, colors);
+
+}
+
+const fillRight = (ctx, size, mid, start=0, colors) => {
+
+    const end = start + size;
+    fillTopLeft(ctx, size, mid, end, 0, colors)
+    fillBottomRight(ctx, size, mid, end, 0, colors)
+
+}
+
+const singleTile = (ctx, size, colors, start=0) => {
+
+    const mid = start + Math.floor(size / 2)
+    const end = start + size
+
+    drawFillTop(ctx, size, mid, 0, colors)
+
+    drawLine(ctx, start, start, start, end, colors)
+    drawLine(ctx, end, start, end, end, colors)
+
+    fillLeft(ctx, size, mid, 0, colors)
+    fillRight(ctx, size, mid, 0, colors)
+
+}
+
+
+const leftHalfTile = (ctx, size, colors, alternate=false, start=0) => {
+    const xSize = Math.floor(size / 2)
+    const xEnd = start + xSize
+    const yMid = Math.floor(size / 2)
+    const yEnd = start + size
+
+    // top
+    ctx.fillStyle = colors.right;
+
+    ctx.beginPath();
+    ctx.moveTo(start, start);
+    ctx.lineTo(xEnd, yMid);
+    ctx.lineTo(xEnd, start);
+    ctx.closePath();
+
+    ctx.fill();
+
+    // middle
+    ctx.fillStyle = colors.top;
+
+    ctx.beginPath();
+    ctx.moveTo(start, start);
+    ctx.lineTo(xEnd, yMid);
+    ctx.lineTo(start, yEnd);
+    ctx.closePath();
+
+    ctx.fill();
+
+    // bottom
+    ctx.fillStyle = colors.left;
+
+    ctx.beginPath();
+    ctx.moveTo(xEnd, yMid);
+    ctx.lineTo(xEnd, yEnd);
+    ctx.lineTo(start, yEnd);
+    ctx.closePath();
+
+    ctx.fill();
+
+}
+
+export { drawLine, drawFillTop, fillBottomLeft,
+    fillTopRight, fillLeft, fillBottomRight, 
+    fillTopLeft, fillRight, singleTile, leftHalfTile
+}
