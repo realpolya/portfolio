512e08ea9cdf23dee222179af54d2dd6e98f17ca
realpolya <ps2780@columbia.edu>
2025-01-10 16:15:54 -0500

    finalize cube tiles

--- app.js
+++ app.js
@@ -21,14 +21,11 @@ const CUBES = 60
 let tileSize = 200; // must correspond with css file
 
 let viewWidth = window.innerWidth;
-let viewHeight = window.innerHeight; // defines number of rows TODO: scrollable site?
 
 let tileCount = 5.5;
 
 /*-------------------------------- Cached Elements --------------------------------*/
 
-const canvasDiv = document.getElementById('canvases')
-const secondCanvasDiv = document.getElementById('secondRow')
 const newCanvases = document.getElementById('newCanvases')
 
 /*-------------------------------- Functions --------------------------------*/
@@ -95,14 +92,7 @@ const rowDiv = (parentEl) => {
     return rowDivvy;
 }
 
-const updateTile = (width, tileCount) => {
-
-    console.log("width is ", width, "tileCount is ", tileCount, "result is ", 
-        Math.floor(width / tileCount)
-    )
-    return Math.floor(width / tileCount)
-
-}
+const updateTile = (width, tileCount) => Math.floor(width / tileCount);
 
 const numberOfTiles = (width) => {
     let count;
@@ -123,20 +113,7 @@ const renderCubes = () => {
     tileCount = numberOfTiles(viewWidth)
     tileSize = updateTile(viewWidth, tileCount)
 
-    canvasDiv.style.width = `${viewWidth}px`
-    secondCanvasDiv.style.width = `${viewWidth}px`
-    canvasDiv.style.height = `${tileSize}px`
-    secondCanvasDiv.style.height = `${tileSize}px`
-
-    console.log("tile size is ", tileSize)
-    console.log("canvas div ", canvasDiv.style.width)
-    console.log("canvas div height ", canvasDiv.style.height)
-    // tileSize = 200
-
     const rows = CUBES / Math.floor(tileCount)
-    console.log("rows are ", rows)
-
-    const totalHeight = rows * viewHeight
 
     let alter = false
     for (let i = 0; i < rows; i++) {
@@ -147,22 +124,7 @@ const renderCubes = () => {
         createRow(newRowDiv, alter, tileSize, tileCount)
 
         alter = !alter
-        console.log("alter is ", alter)
-
     }
-
-    // for (let i = 0; i < rows; i++) {
-    //     if (!alter) {
-    //         createRow(canvasDiv, false, tileSize, tileCount)
-    //     } else if (alter) {
-    //         createRow(secondCanvasDiv, true, tileSize, tileCount)
-    //     }
-    //     alter = !alter
-    //     console.log("alter is ", alter)
-    // }
-
-    // createRow(canvasDiv, false, tileSize, tileCount)
-    // createRow(secondCanvasDiv, true, tileSize, tileCount)
 
 
 }
@@ -179,12 +141,10 @@ window.addEventListener("load", () => {
 })
 
 window.addEventListener("resize", () => {
-    console.log("window resized ", window.innerWidth, window.innerHeight)
+
     viewWidth = window.innerWidth;
     viewHeight = window.innerHeight;
-
-    canvasDiv.innerHTML = ""
-    secondCanvasDiv.innerHTML = ""
+    
     newCanvases.innerHTML = ""
     renderCubes();
 })
